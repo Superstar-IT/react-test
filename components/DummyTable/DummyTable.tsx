3eb2d7f6596f2ea5fa5554faffaef44fe67c3b80
Superstar <freewebdev0213@gmail.com>
2020-09-01 20:27:39 +0800

    Test Submit

--- components/DummyTable/DummyTable.tsx
+++ components/DummyTable/DummyTable.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -6,11 +6,32 @@ import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
-
+import { Button } from '@material-ui/core/'
 const DummyTable = (): JSX.Element => {
+  const [tableData, setTableData] = useState([])
+
   useEffect(() => {
     // make an api request for an http://localhost:3000/api/table endpoint
+    const url = 'http://localhost:3000/api/table'
+    const accessToken = localStorage.getItem('ddnxt:token')
+    fetch(url, {
+      method: 'get',
+      headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${accessToken}` },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.rows && result.rows.length > 0) {
+          setTableData(result.rows)
+        }
+      })
   }, [])
+
+  const handleDelete = (item, index) => {
+    if (confirm(`Do you really remove the ${item.title}?`)) {
+      tableData.splice(index, 1)
+    }
+    setTableData([...tableData])
+  }
 
   return (
     <TableContainer component={Paper}>
@@ -18,10 +39,27 @@ const DummyTable = (): JSX.Element => {
         <TableHead>
           <TableRow>
             <TableCell>ID</TableCell>
-            <TableCell align="right">Title</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Action</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody></TableBody>
+        <TableBody>
+          {tableData.length > 0 &&
+            tableData.map((item, index) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.id}</TableCell>
+                <TableCell>{item.title}</TableCell>
+                <TableCell>
+                  <Button
+                    style={{ color: 'red', borderWidth: 1, borderColor: 'red', borderStyle: 'outset' }}
+                    onClick={() => handleDelete(item, index)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
       </Table>
     </TableContainer>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
 import { Paper, Grid, TextField, Button } from '@material-ui/core'
+import { useState } from 'react'
 
 const Home = (): JSX.Element => {
-  // const handleSubmit = (): void => {
-  // make an api request for an http://localhost:3000/api/login endpoint
-  // body request example { username: 'test', password: 'test }
-  // please, display server errors somewhere
-  // username 'ddi' will return token, otherwise -- error message
-  // }
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (): void => {
+    // make an api request for an http://localhost:3000/api/login endpoint
+    // body request example { username: 'test', password: 'test }
+    // please, display server errors somewhere
+    // username 'ddi' will return token, otherwise -- error message
+    const url = 'http://localhost:3000/api/login'
+    const data = {
+      username: username,
+      password: password,
+    }
+    fetch(url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.token) {
+          localStorage.setItem('ddnxt:token', result.token)
+          window.location.href = '/table'
+        } else if (result.error) {
+          alert(result.error)
+        }
+      })
+  }
 
   return (
     <Grid container spacing={0} direction="column" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
@@ -14,16 +37,39 @@ const Home = (): JSX.Element => {
         <Grid container>
           <Grid container spacing={8} alignItems="flex-end">
             <Grid item md={true} sm={true} xs={true}>
-              <TextField id="username" label="Username" type="text" fullWidth autoFocus required />
+              <TextField
+                id="username"
+                label="Username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                fullWidth
+                autoFocus
+                required
+              />
             </Grid>
           </Grid>
           <Grid container spacing={8} alignItems="flex-end">
             <Grid item md={true} sm={true} xs={true}>
-              <TextField id="password" label="Password" type="password" fullWidth required />
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth
+                required
+              />
             </Grid>
           </Grid>
           <Grid container justify="center" style={{ marginTop: '10px' }}>
-            <Button type="submit" variant="outlined" color="primary" style={{ textTransform: 'none' }}>
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              style={{ textTransform: 'none' }}
+              onClick={() => handleSubmit()}
+            >
               Login
             </Button>
           </Grid>
